6f4e3795c7a9eeb013cfae13b7f8be19d8e9d9bb
JebetIvy <138754525+IvyJebet@users.noreply.github.com>
2023-11-15 17:41:50 +0300

    betty.c File

---
+++ betty.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * main - Determine whether a nrandom number is zero, positive or negative.
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	return (0);
+}
